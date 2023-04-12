23198e35af9e13bb0f740fca921de1b144e05346
pierlot5u <maximepierlot@gmail.com>
2023-04-12 15:20:12 +0200

    Push fxml + début controler

--- src/main/java/univ_lorraine/iut/java/privatechat/controller/LoginController.java
+++ src/main/java/univ_lorraine/iut/java/privatechat/controller/LoginController.java
@@ -2,12 +2,37 @@ package univ_lorraine.iut.java.privatechat.controller;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import univ_lorraine.iut.java.privatechat.App;
 
 public class LoginController {
 
     @FXML
+    TextField txtusername;
+    @FXML
+    TextField txtpassword;
+    @FXML
     private void login() throws IOException {
+
+        if (txtusername.getText().isEmpty()) {
+            throw new IllegalArgumentException("Le champ username est vide");
+        }
+        if (txtusername.getText().length() < 6) {
+            throw new IllegalArgumentException("Le username doit contenir au moins 6 caractères");
+        }
+
+
+        if (txtpassword.getText().isEmpty()) {
+            throw new IllegalArgumentException("Le champ password est vide");
+        }
+        if (txtpassword.getText().length() < 8) {
+            throw new IllegalArgumentException("Le password doit contenir au moins 8 caractères");
+        }
+
         App.setRoot("chat");
     }
+
+
+
+
 }
